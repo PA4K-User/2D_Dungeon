a2107f0ff1bfd39463ead5b9bce22a61a2ffc8aa
PA4K <siwaphon08@gmail.com>
2022-07-20 13:58:33 +0700

    Enemy Setup

---
+++ Assets/Scripts/Enemies/EnemyDetailsSO.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyDetails_", menuName = "Scriptable Objects/Enemy/EnemyDetails")]
+public class EnemyDetailsSO : ScriptableObject
+{
+    [Space(10)]
+    [Header("BASE ENEMY DETAILS")]
+    public string enemyName;
+    public GameObject enemyPrefab;
+
+    #region Validation
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEmptyString(this, nameof(enemyName), enemyName);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(enemyPrefab), enemyPrefab);
+    }
+#endif
+    #endregion
+}
